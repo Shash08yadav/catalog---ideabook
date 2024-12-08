9736a14b302b60ee69833d735b67212f3f2ec626
Shashwat Yadav <140447110+Shash08yadav@users.noreply.github.com>
2024-12-08 23:44:47 +0530

    sanity added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,46 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({ searchParams} : {
+  searchParams: Promise<{query? : string}>
+}){
+  const query=(await searchParams).query;
+  const posts =[{
+    _createdAt:new Date(),
+    views:55,
+    author:{_id:1,name:'shashwat'},
+    _id:1,
+    description:'This is a description.',
+    image:'https://picsum.photos/200/300',
+    ctegory:"Robots",
+    title:"We Robots",
+  }];
+
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello, I am READY TO GO.</h1>
-      <p>Lets create signup page/ signin page</p>
-      <Button>Click me</Button>
-    </main>
+   
+    <>
+    <section className="pink_container">
+      <h1 className="heading">Pitch your Startup ,<br/>Connect With<br/>Enterpreneurs</h1>
+      <p className="sub_heading !max-w-3xl">submit ideas, vote on pitches,and get Noticed in Virtual Competitions.</p>
+    <SearchForm query={query}/>
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : 'All Startups'}
+
+      </p>
+      <ul className="mt-7 card_grid">
+  {posts?.length > 0 ? (
+    posts.map((post: StartupCardType, index: number) => (
+      <StartupCard key={post?._id} post={post} />
+    ))
+  ) : (
+    <p className="no-results">No startups found</p>
+  )}
+</ul>
+
+
+    </section>
+    </>
   );
 }
---
+++ components/SearchForm.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import SearchFormReset from './SearchFormReset';
+import Form from "next/form";
+import { Search, SearchSlashIcon } from 'lucide-react';
+const SearchForm = ({query}:{query?: string}) => {
+  return (
+    <Form  action= "/" scroll={false} className="search-form">
+        <input 
+            name="query"
+            defaultValue={query}
+            className='search-input'
+            placeholder='Search Startups'
+        />
+        <div className='flex-gap-2'>
+            {query && <SearchFormReset />}
+            <button type='submit' className='search-btn text-white'>
+                <Search className='size-5'></Search>
+            </button>
+        </div>
+    </Form>
+  )
+}
+
+export default SearchForm
